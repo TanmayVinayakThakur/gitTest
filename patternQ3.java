eb977725fd3da86cd83300d545eddfdfa3608d47
TanmayVinayakThakur <71790710+TanmayVinayakThakur@users.noreply.github.com>
2024-06-02 22:34:44 +0530

    Second pattern Q

---
+++ patternQ3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class patternQ3 {
+    public static void main(String args[]){
+        Scanner s2= new Scanner(System.in);
+        int n= s2.nextInt();
+        for(int i=1; i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+
+    }
+    
+}
